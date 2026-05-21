cb9ff2c323a9c477172b26adec463be7291e6453
NachoC19 <ignaciocendon04@gmail.com>
2026-05-21 12:22:00 -0300

    Se inicializaron los parametros de la clase Princesa

---
+++ src/juego/Princesa.java
@@ -0,0 +1,15 @@
+package juego;
+import java.awt.Image;
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class Princesa {
+
+private double x;
+private double y;
+private double velY; //velocidad vertical (gravedad y salto)
+private int vidas;
+private boolean enSuelo; //true cuando esta parada sobre algo
+private Image imagen;
+
+}

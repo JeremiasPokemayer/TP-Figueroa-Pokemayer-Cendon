31530b4ad4c59915fc8ced4c3aa4bf3a6fc394e9
Jeremias Pokemayer <jerepoke2016@gmail.com>
2026-05-23 16:48:15 -0300

    cree clase isla, la genere en la clase juego y ya son visibles

---
+++ src/juego/Isla.java
@@ -0,0 +1,56 @@
+package juego;
+import java.awt.Color;
+import entorno.Entorno;
+
+public class Isla {
+	private int x;
+	private int y;
+	private int ancho;
+	private int alto;
+	public Isla(int x,int y,int ancho,int alto) {
+		this.x=x;
+		this.y=y;
+		this.ancho=ancho;
+		this.alto=alto;
+	}
+	public void dibujarIsla(Entorno entorno, int camaraX) {
+		entorno.dibujarRectangulo(this.x - camaraX, this.y, this.ancho, this.alto, 0, Color.BLUE);
+	}
+	
+	public boolean alejaDe(Isla otra) {
+		int margenX = 80;
+		int margenY = 60;
+		
+		return
+			this.x - this.ancho / 2 - margenX < otra.x + otra.ancho / 2 &&
+			this.x + this.ancho / 2 + margenX > otra.x - otra.ancho / 2 &&
+			this.y - this.alto / 2 - margenY < otra.y + otra.alto / 2 &&
+			this.y + this.alto / 2 + margenY > otra.y - otra.alto / 2;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getAncho() {
+		return ancho;
+	}
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+	public int getAlto() {
+		return alto;
+	}
+	public void setAlto(int alto) {
+		this.alto = alto;
+	}
+	
+}
--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,27 +1,59 @@
 package juego;
 
-
-import java.awt.Color;
+import java.util.Random;
 
 import entorno.Entorno;
 import entorno.InterfaceJuego;
 
 public class Juego extends InterfaceJuego
 {
+	public static class Aleatorio {
+		private static Random random= new Random();
+		
+		public static int numAleatX() {
+			return(random.nextInt(3000));
+		}
+	
+		public static int numAleatY() {
+			return (random.nextInt(350)+200);
+		}
+		
+		public static int numAleatAncho() {
+			return (random.nextInt(490)+250);
+		}
+	}
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	
 	// Variables y métodos propios de cada grupo
 	// ...
+	private Isla[] isla;
+	private int camaraX; // variable para mover la camara conforme al personaje. 
 	
 	Juego()
 	{
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
-		
 		// Inicializar lo que haga falta para el juego
-		// ...
-
+		this.isla = new Isla[10];
+		
+		this.isla[0]= new Isla(200,565,400,100);
+		this.isla[1]= new Isla(650,565,300,100);
+		this.isla[2]= new Isla(1200,565,500,100); 	// islas base
+		for(int i = 3;i<this.isla.length;i++) {
+			Isla nueva = new Isla(Aleatorio.numAleatX(),Aleatorio.numAleatY(),Aleatorio.numAleatAncho(),40); //islas flotantes
+			boolean colisiona=true;
+			while(colisiona) {
+				colisiona=false;
+				for(int j = 0; j < i;j++) {
+					if(nueva.alejaDe(this.isla[j])) { //chequea que las islas no colisionen, si colisionan crea una nueva
+						colisiona=true;
+						nueva= new Isla(Aleatorio.numAleatX(),Aleatorio.numAleatY(),Aleatorio.numAleatAncho(),40);
+					}
+				}
+			}
+			this.isla[i]=nueva;
+		}
 		// Inicia el juego!
 		this.entorno.iniciar();
 	}
@@ -36,7 +68,9 @@ public class Juego extends InterfaceJuego
 	{
 		// Procesamiento de un instante de tiempo
 		// ...
-		
+		for(int i=0;i<isla.length;i++) {
+			isla[i].dibujarIsla(entorno,camaraX);
+		}
 	}
 	
 
